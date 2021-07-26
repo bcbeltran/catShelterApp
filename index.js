1320600841d3e743f3dd3b1d174e8c7ee81c7741
bcbeltran <benjamin.beltran37@gmail.com>
2021-07-26 16:16:24 -0700

    mod4sess3

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const port = 3000;
+const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/plain";
-    });
-});
+    
+    for (let handler of handlers) {
+        if (!handler(req,res)) {
+            break;
+        }
+    }
+}).listen(port);
